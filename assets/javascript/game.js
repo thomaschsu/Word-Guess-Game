b605af39fd3eea2ba01928e63bb02224ad152bd4
Tom Hsu <thomaschsu@gmail.com>
2018-05-22 22:36:33 -0400

    Removed letters variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 // Variables
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var guessedLetters = [];
 var wins = 0;
 var losses = 0;
